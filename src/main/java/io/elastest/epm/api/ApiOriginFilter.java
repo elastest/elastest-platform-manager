9463e5dae2576e3ad05e45d5f99c1a186cf0d807
Michael Pauls <michael.pauls@tu-berlin.de>
2017-07-19 23:58:12 +0200

    add PATCH

--- src/main/java/io/elastest/epm/api/ApiOriginFilter.java
+++ src/main/java/io/elastest/epm/api/ApiOriginFilter.java
@@ -14,7 +14,7 @@ public class ApiOriginFilter implements javax.servlet.Filter {
       throws IOException, ServletException {
     HttpServletResponse res = (HttpServletResponse) response;
     res.addHeader("Access-Control-Allow-Origin", "*");
-    res.addHeader("Access-Control-Allow-Methods", "GET, POST, DELETE, PUT");
+    res.addHeader("Access-Control-Allow-Methods", "GET, POST, DELETE, PUT, PATCH");
     res.addHeader("Access-Control-Allow-Headers", "Content-Type");
     chain.doFilter(request, response);
   }
