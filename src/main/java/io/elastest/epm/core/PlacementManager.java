ab91259f69f08712bf093a9140d1289154590a40
radoslavvlaskovski <radoslav.vlaskovsky@gmail.com>
2018-04-19 14:09:39 +0200

    added placement manager

---
+++ src/main/java/io/elastest/epm/core/PlacementManager.java
@@ -0,0 +1,34 @@
+package io.elastest.epm.core;
+
+import io.elastest.epm.exception.NotFoundException;
+import io.elastest.epm.model.KeyValuePair;
+import io.elastest.epm.model.PoP;
+import io.elastest.epm.repository.PoPRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class PlacementManager {
+
+    @Autowired private PoPRepository poPRepository;
+
+    public PoP choosePoPByType(String type) throws NotFoundException {
+        /*
+        In the future a better automatic placement will be integrated into this code
+         */
+        PoP poP = null;
+        Iterable<PoP> pops = poPRepository.findAll();
+        for (PoP pop : pops) {
+            for (KeyValuePair kvp : pop.getInterfaceInfo()) {
+                if (kvp.getKey().equals("type") && kvp.getValue().equals(type)) {
+                    poP = pop;
+                    break;
+                }
+            }
+        }
+
+        if (poP == null || poP.getInterfaceEndpoint() == null)
+            throw new NotFoundException("No " + type + " pop was registered!");
+        return poP;
+    }
+}
--- src/main/java/io/elastest/epm/repository/PoPRepositoryImpl.java
+++ src/main/java/io/elastest/epm/repository/PoPRepositoryImpl.java
@@ -1,5 +1,6 @@
 package io.elastest.epm.repository;
 
+import io.elastest.epm.core.PlacementManager;
 import io.elastest.epm.exception.NotFoundException;
 import io.elastest.epm.model.KeyValuePair;
 import io.elastest.epm.model.PoP;
@@ -8,24 +9,11 @@ import org.springframework.transaction.annotation.Transactional;
 
 public class PoPRepositoryImpl implements PoPRepositoryCustom {
 
-  @Autowired private PoPRepository poPRepository;
+  @Autowired private PlacementManager placementManager;
 
   @Override
   @Transactional
   public PoP findPoPForType(String type) throws NotFoundException {
-    PoP composePoP = null;
-    Iterable<PoP> pops = poPRepository.findAll();
-    for (PoP pop : pops) {
-      for (KeyValuePair kvp : pop.getInterfaceInfo()) {
-        if (kvp.getKey().equals("type") && kvp.getValue().equals(type)) {
-          composePoP = pop;
-          break;
-        }
-      }
-    }
-
-    if (composePoP == null || composePoP.getInterfaceEndpoint() == null)
-      throw new NotFoundException("No " + type + " pop was registered!");
-    return composePoP;
+      return placementManager.choosePoPByType(type);
   }
 }
